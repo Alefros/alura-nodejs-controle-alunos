829937ec7f23176c1b46619a00b86f5cc96d025d
Alefros <alef.r.oliveira@gmail.com>
2021-06-09 01:18:49 -0300

    Adição do método que trás todas as matriculas confirmadas de um estudante

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -177,6 +177,17 @@ class PessoaController{
         }
     }
 
+    static async pegaMatriculas(req, res){
+        const idEstudante = req.params.estudanteId
+        try {
+            const pessoa = await database.Pessoas.findOne({ where: { id: Number(idEstudante) } })
+            const matriculas = await pessoa.getAulasMatriculadas()
+            return res.status(200).json(matriculas)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 
 
 }
