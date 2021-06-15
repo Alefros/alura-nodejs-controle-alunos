3f3186a0eed28a18bd937d952a72bd200e54d45f
Alefros <alef.r.oliveira@gmail.com>
2021-06-14 22:49:29 -0300

    Criação do ponto de entrada dos serviços

---
+++ api/services/index.js
@@ -0,0 +1,10 @@
+const PessoasServices = require('./PessoasServices')
+const TurmasServices = require('./TurmasServices')
+const NiveisServices = require('./PessoasServices')
+
+
+module.exports = {
+    PessoasServices: PessoasServices,
+    TurmasServices: TurmasServices,
+    NiveisServices: NiveisServices
+}
